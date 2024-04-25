59a11249edca6266f0431f83e8622c30ac42d0e3
Priyabrata <sahoopriya732@gmail.com>
2024-04-25 18:56:04 +0530

    place order done stripe payment added

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,11 @@
+import { placeOrder, verifyOrder } from "../controllers/orderController.js";
+import express from 'express'
+import authMiddleware from '../middleware/auth.js'
+
+const orderRouter =express.Router()
+
+orderRouter.post('/place',authMiddleware,placeOrder)
+orderRouter.post('/verify',verifyOrder)
+
+
+export default orderRouter
