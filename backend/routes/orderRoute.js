37910563db528c6de8d1f7614cf62b758faf40b8
Priyabrata <sahoopriya732@gmail.com>
2024-04-25 23:35:10 +0530

    Display orders in admin panel is done

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,4 +1,4 @@
-import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
+import { listOrders, placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
 import express from 'express'
 import authMiddleware from '../middleware/auth.js'
 
@@ -7,6 +7,7 @@ const orderRouter =express.Router()
 orderRouter.post('/place',authMiddleware,placeOrder)
 orderRouter.post('/verify',verifyOrder)
 orderRouter.post('/userorders',authMiddleware,userOrders)
+orderRouter.get('/list',listOrders)
 
 
 export default orderRouter
